69e0d3154633ec2ef54f6cf24b3e5fa2366175b7
tristan <tristan.hascoet@gmail.com>
2020-08-11 19:33:29 +0900

    Add evaluation and submission notebooks

--- road/config.py
+++ road/config.py
@@ -1,3 +1,5 @@
 data_path = "/home/tristan/road_project/workspace/data/RoadDamage/train/"
 submission_data_path = "/home/tristan/road_project/workspace/data/RoadDamage/test1/"
+submission_path = "/home/tristan/road_project/workspace/Damage Detection/submissions/"
+model_path = "/home/tristan/road_project/workspace/Damage Detection/Detection/Torchvision/Checkpoints"
 LOG_DIR = "/home/tristan/road_project/workspace/tensorboard"
